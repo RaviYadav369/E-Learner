6a4e458426b7f9ce6e3f3dce0037c2246bf30294
Ravi Yadav <shortanswer12345@gmail.com>
2023-10-10 22:36:35 +0530

    adding different models

--- lib/models/course.model.ts
+++ lib/models/course.model.ts
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   title: {
     type: String,
     require: true,
   },
   description: String,
+  imageUrl: String,
+  price: Float32Array,
+  isPublished: Boolean,
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
   createdAt: {
     type: Date,
     default: Date.now,
